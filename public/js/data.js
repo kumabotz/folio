c7f47c0dd045216ebb0644d232dcc7091e841807
kumabotz <kumabotz@gmail.com>
2013-09-08 13:58:24 +0800

    Center player, sail ship and fix scss

---
+++ public/js/data.js
@@ -0,0 +1,33 @@
+var houses = [
+  {
+    name: 'About House',
+    id: '#aboutHouse',
+    width: 200,
+    height: 140,
+    top: 470,
+    left: 50,
+    right: null,
+    door: {
+      width: 36,
+      height: 39,
+      top: 100,
+      left: 41,
+    }
+  }
+];
+
+var notifications = [
+  {
+    id: 1,
+    type: 'error',
+    text: 'You can\'t teleport me here...',
+    img: 'images/error.png'
+  },
+  {
+    id: 2,
+    type: 'info',
+    text: 'If you\'re too lazy to walk, you can teleport yourself by ' +
+        'clicking on the top navigation bar items',
+    img: 'images/information.png'
+  }
+]
